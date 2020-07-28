566ec1afd3de784b506929e6f000b62a2dc217bc
Bowei Du <bowei@google.com>
2020-07-27 17:31:00 -0700

    Add dump of netstat in netstat -s

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@
 set -e
 
 function truncate_logs() {
+  # Get the total number of bytes in the logs.
   local total=$(du -b ${logs}/dump_*.tgz | awk '{x += $1} END{print x}')
-  echo TOTAL $total
+  # Don't truncate if we are under the limit.
   if [[ ${total} -le ${bytesMax} ]]; then
     return
   fi
 
   echo "$(date '+%Y-%m-%dT%H:%M:%SZ') Cleaning logs"
+  # Go through the logs (in alphabetical order), deleting the oldest until
   for f in ${logs}/dump_*.tgz; do
     local sz=$(du -b $f | awk  '{print $1}')
     rm ${f}
@@ -82,7 +84,6 @@ while true; do
   echo "${c}"           | tee -a "${console}"
   echo "_END___ $d $f"  | tee -a "${console}"
   echo "${c}"           > "${logs}/cur/${f}"
-
   if [[ ${useJournal} = 'y' ]]; then
     echo ${c} | systemd-cat -p notice -t "gke[$f]"
   fi
@@ -95,10 +96,37 @@ while true; do
   echo "${c}"           | tee -a "${console}"
   echo "_END___ $d $f"  | tee -a "${console}"
   echo "${c}"           > "${logs}/cur/${f}"
-
   if [[ ${useJournal} = 'y' ]]; then
     echo ${c} | systemd-cat -p notice -t "gke[$f]"
   fi
+
+  # top
+  d=$(date '+%Y-%m-%dT%H:%M:%S')
+  f=top
+  c=$(top -b -n1)
+
+  echo "_BEGIN_ $d $f"  | tee -a "${console}"
+  echo "${c}"           | tee -a "${console}"
+  echo "_END___ $d $f"  | tee -a "${console}"
+  echo "${c}"           > "${logs}/cur/${f}"
+  if [[ ${useJournal} = 'y' ]]; then
+    echo ${c} | systemd-cat -p notice -t "gke[$f]"
+  fi
+
+  # netstat in ns
+  for pid in $(lsns | grep \ net | awk '{print $4}'); do
+    d=$(date '+%Y-%m-%dT%H:%M:%S')
+    f=netstat_${pid}
+    c=$(nsenter --net -t ${pid} netstat -s)
+
+    echo "_BEGIN_ $d $f"  | tee -a "${console}"
+    echo "${c}"           | tee -a "${console}"
+    echo "_END___ $d $f"  | tee -a "${console}"
+    echo "${c}"           > "${logs}/cur/${f}"
+    if [[ ${useJournal} = 'y' ]]; then
+      echo ${c} | systemd-cat -p notice -t "gke[$f]"
+    fi
+  done
 
   tar -czf "${logs}/dump_$(cat /${logs}/cur/date.txt).tgz" -C ${logs}/cur .
 
